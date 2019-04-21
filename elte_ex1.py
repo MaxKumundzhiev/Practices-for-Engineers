1e7fb2c8e99e62eb561c6806e72c9a4f9359587f
Maksim Kumundzhiev <kumundzhiev_maxim@mail.ru>
2019-04-21 12:11:04 +0200

    Eotvos Lorand University enrollment tasks

--- elte_ex1.py
+++ elte_ex1.py
@@ -11,3 +11,7 @@
 # Exercise 1
 # How many times did it occur in the trial, that exactly two 6s were rolled after each other?
 # For example, in sequence 56611166626634416 it occurred twice, that exactly two 6s were thrown after each other.
+
+str = 'Advanced Software for DataScience'
+
+print(str.upper())
--- two_sum.py
+++ two_sum.py
@@ -12,13 +12,15 @@ def twoSum(list, target):
     for i in range(len(list)):
         for j in range(1, len(list)):
             result = list[i] + list[j]
-            if result == target:
-                return result
+            L_i = list[i]
+            L_j = list[j]
+            if (result == target) and (i != j):
+                return (i, j)
             elif result != target:
                 result = 'Fasle'
     return result
 
 
-print((twoSum([2,7,11,15], 22))
+print((twoSum([-1,-2,-3,-4,-5], -8)))
 
 
