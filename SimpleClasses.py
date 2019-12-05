2f1b072266c3c479f93a3c36e1c0ff8a012767b1
Maksim Kumundzhiev <kumundzhievmaxim@gmail.com>
2019-12-05 19:16:42 +0100

    python_practices, added example into classes;

--- SimpleClasses.py
+++ SimpleClasses.py
@@ -45,11 +45,11 @@ class Dog():
         '''define a method which tell the  dog to jump '''
         print(self.name + ' dog jumps')
 
-my_dog = Dog('Dora', 10)
-neighbour_dog = Dog('Charli', 4)
+# my_dog = Dog('Dora', 10)
+# neighbour_dog = Dog('Charli', 4)
 
-print(my_dog.name, my_dog.age, my_dog.command_sit(), my_dog.command_jump())
-print(neighbour_dog.name, neighbour_dog.age, neighbour_dog.command_sit())
+# print(my_dog.name, my_dog.age, my_dog.command_sit(), my_dog.command_jump())
+# print(neighbour_dog.name, neighbour_dog.age, neighbour_dog.command_sit())
 
 
 class Cat():
@@ -59,9 +59,34 @@ class Cat():
         self.color = color
         print('Called the object Cat with name {0} and color {1}'.format(name, color))
 
+# my_cat = Cat()
+# my_cat('Tom', 2)
 
 
-my_cat = Cat()
-my_cat('Tom', 2)
 
+class Counter():
+    '''This class counts some staff'''
+    def __init__(self, initial_count=0):
+        self.count = initial_count
+        print('New counter is created')
 
+    def get(self):
+        return self.count
+
+    def increment(self):
+        self.count += 1
+
+initial_attempt = Counter()
+
+print(initial_attempt.get())
+
+initial_attempt.increment()
+initial_attempt.increment()
+initial_attempt.increment()
+
+print(initial_attempt.get())
+
+second_attempt = Counter(initial_count=89)
+print(second_attempt.get())
+second_attempt.increment()
+print(second_attempt.get())
