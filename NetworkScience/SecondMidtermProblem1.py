82c165acf1e1a3921144b012dbf4ed5ea3aeae15
Maksim Kumundzhiev <kumundzhievmaxim@gmail.com>
2019-12-16 00:12:13 +0100

    NetworkScience, added Second Midterm Task;

---
+++ NetworkScience/SecondMidtermProblem1.py
@@ -0,0 +1,23 @@
+# ------------------------------------------
+# 
+# Program created by Maksim Kumundzhiev
+#
+#
+# email: kumundzhievmaxim@gmail.com
+# github: https://github.com/KumundzhievMaxim
+# -------------------------------------------
+
+"""Description:
+a) Create a random network of 100 nodes and 500 edges! Explain in 2-3 sentences, what are you expecting for the degree distribution of this network!
+b) Write an algorithm, which rewires this network by relocating only one end of a single edge in a step.
+c) Using the rewiring routine, change the network step by step such, that finally the nodes with largest degree
+centrality must have the following degrees: 24, 21, 19, 19, 15, 14. In the first step, when this condition is satisfied, the program must stop.
+d) Plot the degree distribution of the final network!"""
+
+
+#a) Create a random network of 100 nodes and 500 edges! Explain in 2-3 sentences, what are you expecting for the degree distribution of this network!
+
+def random_graph():
+    pass
+
+
