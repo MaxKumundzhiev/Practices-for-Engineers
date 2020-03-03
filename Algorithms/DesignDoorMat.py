74cd3b00977a1a5cb3c30e391720a6cdf0625421
Maksim Kumundzhiev <kumundzhievmaxim@gmail.com>
2020-03-03 10:38:37 +0100

    Algorithms, added DesignDoorMat problem

---
+++ Algorithms/DesignDoorMat.py
@@ -0,0 +1,21 @@
+def yolo(N):
+    M = 3*N
+    index = 0
+    dot = '.'
+    line = '|'
+    while index <= M:
+        #loop controls number of rows
+        #7 --> len of WELCOME
+        first_index_map, last_index_map, middle_index_map = M // 2 - 1, M // 2 + 1, M // 2
+        if (index == first_index_map) or (index == last_index_map):
+            print(dot, end='')
+            index += 1
+        elif index == middle_index_map:
+            print(line, end='')
+            index += 1
+        else:
+            print('-', end='')
+            index += 1
+
+yolo(5)            
+
