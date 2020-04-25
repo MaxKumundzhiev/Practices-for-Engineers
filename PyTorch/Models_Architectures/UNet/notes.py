0a5b44baee46e6d2a89ac762e5870b05892be257
Maksim Kumundzhiev <kumundzhievmaxim@gmail.com>
2020-04-25 13:58:39 +0200

    PyTorch, added initial spread sheet data information, requirements

---
+++ PyTorch/Models_Architectures/UNet/notes.py
@@ -0,0 +1,16 @@
+# ------------------------------------------
+# 
+# Program created by Maksim Kumundzhiev
+#
+#
+# email: kumundzhievmaxim@gmail.com
+# github: https://github.com/KumundzhievMaxim
+# -------------------------------------------
+
+import os
+import pandas as pd
+
+
+
+data = pd.
+
