4501679130f71d9a1a26b9499f7c7a2fc9c43e71
KumundzhievMaxim <kumundzhievmaxim@gmail.com>
2020-08-13 12:16:36 +0200

    restructured project structure.

---
+++ CompaniesPractices/Company10/main/handler.py
@@ -0,0 +1,16 @@
+# ------------------------------------------
+# 
+# Program created by Maksim Kumundzhiev
+#
+#
+# email: kumundzhievmaxim@gmail.com
+# github: https://github.com/KumundzhievMaxim
+# -------------------------------------------
+
+import os
+import json
+import pandas as pd
+from tqdm import tqdm
+
+
+
