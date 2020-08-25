d86e11571a8097e07a1e1f8bdb4d7c9a9ad7a8d5
KumundzhievMaxim <kumundzhievmaxim@gmail.com>
2020-08-25 23:41:38 +0200

    Creted initial projection net module.

---
+++ PyTorch/Architectures/SimCLR/utils/common/projection.py
@@ -0,0 +1,20 @@
+# ------------------------------------------
+# 
+# Program created by Maksim Kumundzhiev
+#
+#
+# email: kumundzhievmaxim@gmail.com
+# github: https://github.com/KumundzhievMaxim
+# -------------------------------------------
+
+"""
+A projection network, P(·), which maps the normalized representation vector r into a vector z = P(r) ∈ RDP
+suitable for computation of the contrastive loss. For our projection network, we use a multi-layer perceptron [18] with
+a single hidden layer of size 2048 and output vector of size DP = 128. We again normalize this vector to lie on the
+unit hypersphere, which enables using an inner product to measure distances in the projection space. The projection
+network is only used for training the supervised contrastive loss. After the training is completed, we discard this
+network and replace it with a single linear layer (for more details see Sec. 4). Similar to the results for self-supervised
+contrastive learning [46, 6], we found representations from the encoder to give improved performance on downstream
+tasks than those from the projection network. Thus our inference-time models contain exactly the same number of
+parameters as their cross-entropy equivalents.
+"""
