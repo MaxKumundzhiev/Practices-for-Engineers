5c3792a59f177179f62fb98f3d9ee3e439b350cb
KumundzhievMaxim <kumundzhievmaxim@gmail.com>
2020-08-26 00:38:21 +0200

    Updated projection

--- PyTorch/Architectures/SimCLR/utils/common/projection.py
+++ PyTorch/Architectures/SimCLR/utils/common/projection.py
@@ -20,8 +20,36 @@ parameters as their cross-entropy equivalents.
 """
 
 import torch.nn as nn
+from efficientnet_pytorch import EfficientNet
 
 
-class ProjectionNet(nn.Module):
-    def __init__(self):
-        super(ProjectionNet).__init__(self)
+class ImageEmbedding (nn.Module):
+    class Identity (nn.Module):
+        def __init__(self): super().__init__ ()
+
+        def forward(self, x):
+            return x
+
+    def __init__(self, embedding_size=1024):
+        super ().__init__ ()
+
+        base_model = EfficientNet.from_pretrained ("efficientnet-b0")
+        internal_embedding_size = base_model._fc.in_features
+        base_model._fc = ImageEmbedding.Identity ()
+
+        self.embedding = base_model
+
+        self.projection = nn.Sequential (
+            nn.Linear (in_features=internal_embedding_size, out_features=embedding_size),
+            nn.ReLU (),
+            nn.Linear (in_features=embedding_size, out_features=embedding_size)
+        )
+
+    def calculate_embedding(self, image):
+        return self.embedding (image)
+
+    def forward(self, X):
+        image = X
+        embedding = self.calculate_embedding (image)
+        projection = self.projection (embedding)
+        return embedding, projection
