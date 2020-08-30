b43612cfc8309196b6232b62da44e21f8ceafc4f
KumundzhievMaxim <kumundzhievmaxim@gmail.com>
2020-08-30 17:45:53 +0200

    small additions

--- CompaniesPractices/Company11/handler.py
+++ CompaniesPractices/Company11/handler.py
@@ -1,10 +1,19 @@
 """
 Create follwoing Neural Net:
-Input size: 4 + 52 
+Input size: 4 + 52 = 56
 Ouput: 3 (softmax)  
 Hidden Layers: 2
 Initialize weight with random numbers
 """
 
+import torch 
+import torch.nn as nn
+import torch.functional as F
 
-import torch
+
+class SampeNet(nn.Module):
+    def __init__(self):
+        super(SampeNet).__init__()
+        
+
+
