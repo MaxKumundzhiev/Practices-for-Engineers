42c9a4977e1a64f5937efe860d6277b2763dbf3d
Maksim Kumundzhiev <kumundzhievmaxim@gmail.com>
2020-10-24 15:54:41 +0200

    Added algos

--- Algorithms/MapReduce.py
+++ Algorithms/MapReduce.py
@@ -29,31 +29,78 @@ Reduce - applies a function of two arguments cumulatively to the elements of an 
     print(reduce(lambda x, y: x - y, data))
 """
 
-# Map
-pets = ['a', 'b', 'c', 'd', 'd']
-print(list(map(str.upper, pets)))
+# # Map
+# pets = ['a', 'b', 'c', 'd', 'd']
+# print(list(map(str.upper, pets)))
+#
+# # circles = [1.1281, 12.1212098, -299912.8]
+# # print(list(map(round, circles, range(1, 3))))
+#
+# # Map and custom zip
+# pets = ['dog', 'cat', 'rabbit', 'owl']
+# weights = [1.1281, 12.1212098, -299912.8]
+#
+# print(zip(pets, weights))
+# print(list(map(lambda pet, weight: (pet, weight), pets, weights)))
+#
+#
+# # Filter
+# numbers = [1, 2, -12, 0, 12, 1e10]
+# print(list(filter(lambda number: number > 0, numbers)))
+#
+# # Find a polindrome
+# dromes = ["demigod", "rewire", "madam", "freer", "anutforajaroftuna", "kiosk"]
+# palindromes = list(filter(lambda word: word == word[::-1], dromes))
+# print(palindromes)
+#
+# # Reduce
+# data = [2, 3, 4, 5, 6, 7, 8, 9]
+# print(reduce(lambda x, y: x - y, data))
 
-# circles = [1.1281, 12.1212098, -299912.8]
-# print(list(map(round, circles, range(1, 3))))
-
-# Map and custom zip
-pets = ['dog', 'cat', 'rabbit', 'owl']
-weights = [1.1281, 12.1212098, -299912.8]
-
-print(zip(pets, weights))
-print(list(map(lambda pet, weight: (pet, weight), pets, weights)))
 
 
-# Filter
-numbers = [1, 2, -12, 0, 12, 1e10]
-print(list(filter(lambda number: number > 0, numbers)))
+"""
+Realisation of MAP REDUCE paradigm.
+"""
 
-# Find a polindrome
-dromes = ["demigod", "rewire", "madam", "freer", "anutforajaroftuna", "kiosk"]
-palindromes = list(filter(lambda word: word == word[::-1], dromes))
-print(palindromes)
+# Map stage
 
-# Reduce
-data = [2, 3, 4, 5, 6, 7, 8, 9]
-print(reduce(lambda x, y: x - y, data))
+import sys
+from operator import itemgetter
 
+for line in sys.stdin:
+    line = line.strip()
+    words = line.split()
+
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        print ('%s\t%s' % (word, 1))
+
+
+# Reduce stage
+current_word = None
+current_count = 0
+word = None
+
+for line in sys.stdin:
+    line.strip()
+    word, count = line.split('\t', 1)
+
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            print('%s\t%s' % (current_word, current_count))
+        current_count = count
+        current_word = word
+
+if current_word == word:
+    print('%s\t%s' % (current_word, current_count))
